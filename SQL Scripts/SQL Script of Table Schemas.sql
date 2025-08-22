d22b7974a8067eb666972ea5dc6dbc3b405b1b8b
gauravsingh9891 <gauravhacker8800@gmail.com>
2025-08-22 17:23:15 +0530

    Adding Script of Regional_Center Entity and their screenshot along with data

--- SQL Scripts/SQL Script of Table Schemas.sql
+++ SQL Scripts/SQL Script of Table Schemas.sql
@@ -3,3 +3,16 @@ Create Database College
 
 --Using Database
 use CollegeDB
+
+/* Regional_Center Entity */
+Create Table Regional_Center
+(
+	RC_Code	int Primary key,
+	Name Varchar(50) Not Null,
+	Location Varchar(500) Not Null,
+	Contac_No varchar(100) Not Null,
+	Email_Id varchar(50) Not Null,
+	Status bit default 1
+)
+
+Select * From Regional_Center
