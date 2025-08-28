f52a249b3ff2de0d538d57575024f3a641cdd463
gauravsingh9891 <gauravhacker8800@gmail.com>
2025-08-28 07:31:23 +0530

    Fixing Bug in SP_GetEnrollmentNo Stored Procedure

--- Stored Procedures Scripts/Student ID Generator Stored Procedure.sql
+++ Stored Procedures Scripts/Student ID Generator Stored Procedure.sql
@@ -40,7 +40,7 @@ Begin
 
 	--Logic To Generate 10 Digit Enrollment Number
 	DECLARE @LastSeq int
-	SET @LastSeq=Cast((SELECT ISNULL(Max(CAST(Right(StudentID,6) AS int)),0) From Students Where Left(StudentID,4)='2504') As int)
+	SET @LastSeq=Cast((SELECT ISNULL(Max(CAST(Right(StudentID,6) AS int)),0) From Students Where Left(StudentID,4)=@Prefix) As int)
 	SET @LastSeq=@LastSeq+1
 	SET @NewEnrollNo=(@Prefix+Right('00000'+Cast(@LastSeq AS Varchar),6))
 End
