e028f0506d079763efd4b35f09b0711776eaae2f
gauravsingh9891 <gauravhacker8800@gmail.com>
2025-08-28 06:36:05 +0530

    Adding Students,Programme,Semesters,Subject,Grades Tables in Entity SQL Script File

---
+++ SQL Scripts/Entity SQL Script.sql
@@ -0,0 +1,68 @@
+Create database College_DB
+Use College_DB
+
+-- Students Table
+CREATE TABLE Students (
+    StudentID Varchar(10) PRIMARY KEY,
+    FullName Varchar(100) Not Null Check(Len(FullName)>=3),
+	DOB Date Not Null Check(Datediff(YY,DOB,Getdate())>15),
+	Gender Char(1) Not Null Check(Gender IN ('M','F','T')),
+	Contact_No Varchar(13) Not Null Check(Len(Contact_No)>=10 And Len(Contact_No)<=13),
+	Email_ID Varchar(30),
+    EnrollmentYear varchar(4) Default Cast(Year(Getdate()) As Varchar),
+	Programme_ID Varchar(50) references Programme(Programme_ID),
+	Status bit Default 1
+);
+
+
+Select * From Students
+
+--Programme Entity
+Create Table Programme
+(
+Programme_ID Varchar(50) Primary Key,
+Programme_Name varchar(100) not null,
+Level varchar(50) not null,
+Programme_Code int identity(1,1),
+Duration varchar(30) not null,
+Total_Semester int not null check(Total_Semester>0),
+Medium varchar(30) not null,
+Fee smallmoney check(Fee>=0),
+Status bit default 1
+)
+
+Select * From Programme
+
+-- Semesters Table
+CREATE TABLE Semesters (
+    SemesterID INT PRIMARY KEY,
+    SemesterName VARCHAR(50)
+);
+
+--Insert Command
+INSERT INTO Semesters VALUES (1, 'Semester 1'), (2, 'Semester 2'),(3, 'Semester 1'), (4, 'Semester 2'),(5, 'Semester 1'), (6, 'Semester 2');
+
+Select * From Semesters
+
+/*Subject Entity*/
+Create Table Subject
+(
+Subject_ID varchar(50) primary key,	
+Subject_Name varchar(100) not null,	
+SemesterID INT references Semesters(SemesterID),
+Credits int Not Null,
+Programme_ID varchar(50) references Programme(Programme_ID),
+Status bit default 1
+)
+
+Select * From Subject
+
+-- Grades Table
+CREATE TABLE Grades (
+    GradeID INT PRIMARY KEY IDENTITY,
+    StudentID Varchar(10) REFERENCES Students(StudentID),
+    Subject_ID varchar(50) REFERENCES Subject(Subject_ID),
+    MarksObtained DECIMAL(5,2),
+    Grade CHAR(2),
+    GradePoint DECIMAL(3,2)
+);
