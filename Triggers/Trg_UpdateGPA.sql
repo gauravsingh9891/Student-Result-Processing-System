386b77919c3004dc3490fbec99f1da478e38a3d3
gauravsingh9891 <gauravhacker8800@gmail.com>
2025-08-29 18:19:24 +0530

    Adding Trg_UpdateGPA Trigger Script in Triggers

---
+++ Triggers/Trg_UpdateGPA.sql
@@ -0,0 +1,22 @@
+/*
+	Purpose of Trg_UpdateGPA Trigger :- When you insert the Student Marks based on Student Id and Subjects of Course 
+	                                    in which he/she enrolled by using SP_InsertGrade Stored Procedure then this 
+										trigger will automatically calculate GPA and also auto update the GPA when new 
+										subject marks enter or updated based on Semester and Student ID wise and insert
+										or update the GPA in GPA Table
+*/
+Create OR Alter Trigger Trg_UpdateGPA
+ON Grades
+AFTER Insert,Update
+As
+Begin
+	SET NOCOUNT ON;
+	Merge GPA As Target Using (
+		Select G.StudentID,S.SemesterID,Round(SUM(G.GradePoint*S.Credits)/Sum(S.Credits),2) AS GPA
+		From Grades as G join Subject as S ON G.Subject_ID=S.Subject_ID Group By G.StudentID,S.SemesterID
+	) As Source ON Target.StudentID=Source.StudentID AND Target.SemesterID=Source.SemesterID
+	When Matched Then
+		Update SET GPA=Source.GPA
+	When Not Matched Then
+		Insert(StudentID,SemesterID,GPA) Values(Source.StudentID,Source.SemesterID,Source.GPA);
+End;
